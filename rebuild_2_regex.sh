de3f75f9bc8c5fcdc9744f74adcb045134d2d007
root <root@ember.lan>
2011-06-22 12:09:32 +1200

    sort regex output

--- rebuild_2_regex.sh
+++ rebuild_2_regex.sh
@@ -2,6 +2,7 @@ sed -e '/^\s*#/d' \
 	-e '/^\s*$/d' \
 	-e 's/$/($|[[:space:]]|[^[:alpha:]]($|[[:space:][:digit:]]))/' \
 	/root/rebuilder/rebuild.txt \
+	| sort -u \
 	| tee /dev/stderr \
 	>| /root/rebuilder/rebuild.regex
 
