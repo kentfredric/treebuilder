8350281d27349408ec4e878cba0caf52e9dbc7f5
Kent Fredric <kentfredric@gmail.com>
2011-07-31 13:44:31 +1200

    update syncer script

--- sync.sh
+++ sync.sh
@@ -3,8 +3,15 @@
 ./rebuild_2_regex.sh
 ./generate_rebuilds.sh
 ./generate_brokens.sh
-diff broken.txt brokens.out
-echo "##"
-shuf -n 20 --random-source=/tmp/rand  rebuilds.out
+echo -e "\e[31m##\e[0m"
+RED="$(  echo -en "\e[31m" )";
+GREEN="$(  echo -en "\e[32m" )";
+RESET="$( echo -en "\e[0m" )";
+diff -Naur broken.txt brokens.out | sed -r -e "s/^-(.*$)/${RED}-\1${RESET}/" -e "s/^\+(.*$)/${GREEN}+\1${RESET}/"
+cat brokens.out rebuilds.out | sort -u > brokens.all
+cat brokens.out rebuilds.out | sort | uniq -d > brokens.duplicate
+cat brokens.out rebuilds.out | sort | uniq -u > brokens.unique
+echo -e "\e[31m##\e[0m"
+#shuf -n 20 --random-source=/tmp/rand  rebuilds.out
 wc -l rebuilds.out 
 
