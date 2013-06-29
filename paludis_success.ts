f544877de880af426d1a5715cebe07034b914ee9
Kent Fredric <kentfredric@gmail.com>
2013-06-29 20:04:00 +1200

    Snapshot Sat Jun 29 20:04:00 NZST 2013

--- paludis_success.ts
+++ paludis_success.ts
@@ -1,1 +1,1 @@
-+2013, 06 June, 29 Saturday, 19:46:28.836276650 394982:138164 605526:2432 ** ~success  install : dev-perl/MooX-Types-MooseLike-0.210.0:0::gentoo
++2013, 06 June, 29 Saturday, 20:02:37.080882311 394982:234988 677320:4832 ** ~success  clean : dev-perl/Test-Kwalitee-1.60.0:0::installed
