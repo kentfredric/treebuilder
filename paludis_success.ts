f54d23fbad0e774a9ae017f699195976517ef9ca
Kent Fredric <kentfredric@gmail.com>
2013-06-29 19:49:45 +1200

    Snapshot Sat Jun 29 19:49:45 NZST 2013

--- paludis_success.ts
+++ paludis_success.ts
@@ -1,1 +1,1 @@
-+2013, 06 June, 29 Saturday, 19:18:34.337971663 357295:26096 ** ~success true task : 
++2013, 06 June, 29 Saturday, 19:46:28.836276650 394982:138164 605526:2432 ** ~success  install : dev-perl/MooX-Types-MooseLike-0.210.0:0::gentoo
