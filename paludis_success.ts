5baa63793d71a40694963d9fc38d18b35b4d5963
Kent Fredric <kentfredric@gmail.com>
2013-08-28 06:05:37 +1200

    Snapshot Wed Aug 28 06:05:36 NZST 2013

--- paludis_success.ts
+++ paludis_success.ts
@@ -1,1 +1,1 @@
-+2013, 08 August, 28 Wednesday, 02:43:48.391352618 830661:59033 843222:13903 ** ~success  install : dev-lang/orc-0.4.18:0::gentoo
++2013, 08 August, 28 Wednesday, 05:58:26.437597210 856320:1128065 208183:9256 ** ~success  install : gnome-base/librsvg-2.37.0:2::gentoo
