562998cf1f7738b0559bcecf6482b907c7c5342d
Kent Fredric <kentfredric@gmail.com>
2013-08-28 02:47:37 +1200

    Snapshot Wed Aug 28 02:47:37 NZST 2013

--- paludis_success.ts
+++ paludis_success.ts
@@ -1,1 +1,1 @@
-+2013, 08 August, 27 Tuesday, 06:34:29.361764050 812337:8882 ** ~success true task : 
++2013, 08 August, 28 Wednesday, 02:43:48.391352618 830661:59033 843222:13903 ** ~success  install : dev-lang/orc-0.4.18:0::gentoo
