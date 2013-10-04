c1740b53ef6aedf93e8a8c9ed63dcd2f78d36063
Kent Fredric <kentfredric@gmail.com>
2013-10-04 15:38:02 +1300

    Snapshot Fri Oct  4 15:38:02 NZDT 2013

--- paludis_success.ts
+++ paludis_success.ts
@@ -1,1 +1,1 @@
-+2013, 10 October, 04 Friday, 15:16:05.247819128 234386:299805 307678:5860 ** ~success  install : dev-util/gdbus-codegen-2.36.4-r1:0::gentoo
++2013, 10 October, 04 Friday, 15:34:41.076095281 234386:411388 311217:111449 ** ~success  install : dev-vcs/subversion-1.7.13:0::gentoo
