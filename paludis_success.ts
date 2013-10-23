8b7a8f60076c5873eb4208987408b9826368ebfb
Kent Fredric <kentfredric@gmail.com>
2013-10-24 06:23:40 +1300

    Snapshot Thu Oct 24 06:23:40 NZDT 2013

--- paludis_success.ts
+++ paludis_success.ts
@@ -1,1 +1,1 @@
-+2013, 10 October, 23 Wednesday, 08:00:16.001606800 953339:2713647 ** ~success true task : 
++2013, 10 October, 24 Thursday, 06:12:25.590622366 699168:1667202 ** ~success true task : 
